90a50f40f3cb191f21ed0169ea9ec97ad84c3b86
Kate Dubitski <kate.dubitski@gmail.com>
2019-01-16 14:11:56 -0500

    added first test and faild it

---
+++ src/components/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Input from './Input';
+import { shallow } from 'enzyme';
+
+describe('Input component', () => {
+  it('renders input', () => {
+    const component = shallow(<Input />);
+    expect(component.exists('input')).toBe(true);
+  });
+});
